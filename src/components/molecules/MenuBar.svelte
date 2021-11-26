13d91781da9b28e8837f3146760052aa28397e99
Aurelien Arino <aurelien.arino@gmail.com>
2021-11-26 14:36:19 +0100

    chore: update statics

--- src/components/molecules/MenuBar.svelte
+++ src/components/molecules/MenuBar.svelte
@@ -8,7 +8,7 @@
 			name: 'introduction',
 		},
 		{
-			href: '/resume.pdf',
+			href: '/aurelien_ARINO_resume.pdf',
 			name: 'resume',
 		},
 		{
@@ -20,31 +20,25 @@
 			name: 'github'
 		}
 	].map(r => {
-		const download = (() => {
-			if (r.href.includes('.pdf')) {
-				return r.href.replace('/', '')
-			}
-			return null
-		})()
 		const target = (() => {
 			const url = safeUrl(r.href)
-			if (url) {
+			if (url || r.href.endsWith('.pdf')) {
 				return '_blank'
 			}
 			return null
 		})()
 
-		return { ...r, download, target }
+		return { ...r, target }
 	})
 </script>
 
 <div class="flex flex-row items-center space-x-4 mx-4">
-	{#each routes as { name, href, download, target }}
+	{#each routes as { name, href, target }}
 		<a
 			class={`text-gray-700 dark:text-white border-b-2 ${
 				name === current ? 'border-blue-500' : 'border-transparent'
 			}`}
-			{href} {download} {target}>{name}</a
+			{href} {target}>{name}</a
 		>
 	{/each}
 </div>
