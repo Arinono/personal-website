4d0ec552535a3ceb1778dedcb19752258df8e8cb
Aurelien Arino <aurelien.arino@gmail.com>
2022-05-13 12:09:32 +0200

    chore: remove spotify cover letter

---
+++ src/routes/not-found.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+	export const prerender = true
+</script>
+
+<script>
+	import { OGlassCardContainer } from '@organisms'
+</script>
+
+<svelte:head>
+	<title>Not found</title>
+</svelte:head>
+
+<div class="flex items-center justify-center w-screen h-screen">
+	<OGlassCardContainer>
+		<span slot="content" class="dark:text-white mt-8">The page you requested is gone 🥹</span>
+	</OGlassCardContainer>
+</div>
