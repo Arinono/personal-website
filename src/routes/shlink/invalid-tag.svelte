3adf80e5b3e93d2706da93c5285d36c8216ef200
Aurelien Arino <aurelien.arino@gmail.com>
2022-06-14 13:01:22 +0200

    feat(shlink): add redirects

---
+++ src/routes/shlink/invalid-tag.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+	export const prerender = true
+</script>
+
+<script>
+	import { OGlassCardContainer } from '@organisms'
+</script>
+
+<svelte:head>
+	<title>Invalid tag</title>
+</svelte:head>
+
+<div class="flex items-center justify-center w-screen h-screen">
+	<OGlassCardContainer>
+		<span slot="content" class="dark:text-white mt-8">This shortened link is invalid 🥹</span>
+	</OGlassCardContainer>
+</div>
---
+++ src/routes/shlink/not-found.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+	export const prerender = true
+</script>
+
+<script>
+	import { OGlassCardContainer } from '@organisms'
+</script>
+
+<svelte:head>
+	<title>Not found</title>
+</svelte:head>
+
+<div class="flex items-center justify-center w-screen h-screen">
+	<OGlassCardContainer>
+		<span slot="content" class="dark:text-white mt-8"
+			>The page behind this shortened link is gone 🥹</span
+		>
+	</OGlassCardContainer>
+</div>
